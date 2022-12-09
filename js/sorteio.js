198b20c7836db455a95c7132397e37b933b6db93
danilo cucharro <danicucharro@outlook.com>
2022-12-09 10:41:15 -0300

    validação no sorteio, ajuste no titulo do header para tela menores, ajuste no tamanho do form para sorteio

--- js/sorteio.js
+++ js/sorteio.js
@@ -1,9 +1,13 @@
 const frmSorteio = document.querySelector("#formSorteio")
 const sorteados = document.querySelector("#timesSorteados")
+const jaFoiSorteado = []
 
 frmSorteio.addEventListener("submit", (e) =>{
     e.preventDefault()
-
-    const sorteio = Math.floor(Math.random() * times.length)
-    sorteados.innerText += `${times[sorteio]} \n`
+    if(times.length == 0){
+        alert("Nenhum time foi cadastrado ainda!")
+    }else{
+        const sorteio = Math.floor(Math.random() * times.length)
+        sorteados.innerText += `${times[sorteio]} \n`
+    }   
 })
