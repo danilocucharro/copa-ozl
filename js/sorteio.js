e0c296cc9e137727508f479298ebf9afa39c3e8d
danilo cucharro <danicucharro@outlook.com>
2022-12-12 10:49:31 -0300

    refatorada no codigo css de divs obsoletas, e agora a aplicação tem responsividade a table

--- js/sorteio.js
+++ js/sorteio.js
@@ -1,15 +1,18 @@
 const frmSorteio = document.querySelector("#formSorteio")
 const sorteados = document.querySelector("#timesSorteados")
 const jaFoiSorteado = []
+let sorteio
 
 frmSorteio.addEventListener("submit", (e) =>{
     e.preventDefault()
     if(times.length == 0){
         alert("Nenhum time foi cadastrado ainda!")
-    }else{
-        const sorteio = Math.floor(Math.random() * times.length)
+    } 
+    else{
+        sorteio = Math.floor(Math.random() * times.length)
         jaFoiSorteado.push(sorteio)
         sorteados.innerText += `${times[sorteio]} \n`
         console.log(jaFoiSorteado)
-    }   
+        console.log(sorteio)
+    }
 })
